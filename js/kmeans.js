98c382217cc19b26ae1ea418dcdc5ecfc4f3349a
Keisuke OSONE <paradi.bass@gmail.com>
2014-09-15 22:02:19 +0900

    プロセスを

--- js/kmeans.js
+++ js/kmeans.js
@@ -1,79 +1,192 @@
 console.log('Hello World!');
 
-var dataNum = 200;
+var dataNum = 100;
 var clusterNum = 4;
 var centroid = true;
 var first = false;
+var finish = false;
+var dataset = []
+var centroidArr = []
 
 clusterColor = ['#a00', '#aa0', '#0a0', '#00a', '#0aa']
 centroidColor = ['#f00', '#ff0', '#0f0', '#00f', '#0ff']
 
-//  Stepを進めるボタン
-var button = d3.select('body')
-                .append('div')
-                .style({
-                    'background-color': 'red',
-                    'height': '80px',
-                    'width': '80px'
-                })
-                .on('click', function () {
-                    if (!first) {
-                        firstCluster();
-                    } else {
-                        if (centroid) {
-                            calCentroid();
-                        } else {
-                            calDistance();
-                        }
-                    }
-                })
+
 
 // 二つの正規分布などからサンプリングしたい
-var svg = d3.select('body').append('svg')
+var svg = d3.select('body')
+    .append('div')
+    .attr({
+        'id': 'main'
+    })
     .style({
+        'margin': '0 auto',
+        'display': '-webkit-box'
+    })
+    .append('svg')
+    .style({
+        'background-color': '#ccc',
         'height': '400px',
         'width': '400px'
     });
 
-dataset = []
+var process = d3.select('#main')
+    .append('div')
+    .attr({
+        'id': 'process'
+    })
+    .style({
+        'background-color': 'gray',
+        'height': '400px',
+        'width': '300px'
+    })
 
-for (var i = 0; i < dataNum; i++) {
-    rand = parseInt((Math.random() * clusterNum), 10);
-    obj = {
-        x: parseInt(normalRand(80 * rand + 100, 20), 10),
-        y: parseInt(normalRand(80 * rand + 100, 20), 10),
-        cluster: parseInt(Math.random() * clusterNum, 10) + 1
+//  Stepを進めるボタン
+process.append('button')
+    .style({
+        'background-color': '#a00',
+        'border-radius': '10px',
+        'color': '#ddd',
+        'height': '40px',
+        'margin': '0 auto',
+        'width': '100px',
+        'text-align': 'center'
+    })
+    .on('click', function () {
+        if (!first) {
+            firstCluster();
+            d3.selectAll('.processDescription')
+                .style({
+                    'background-color': function (d, i) {
+                        if (i == 0) {
+                            return 'white';
+                        } else {
+                            return 'gray';
+                        }
+                    }
+                });
+        } else {
+            if (centroid) {
+                d3.selectAll('.processDescription')
+                .style({
+                    'background-color': function (d, i) {
+                        if (i == 1) {
+                            return 'white';
+                        } else {
+                            return 'gray';
+                        }
+                    }
+                });
+                calCentroid();
+            } else {
+                if (!finish) {
+                    d3.selectAll('.processDescription')
+                    .style({
+                        'background-color': function (d, i) {
+                            if (i == 2) {
+                                return 'white';
+                            } else {
+                                return 'gray';
+                            }
+                        }
+                    });
+                    calDistance();
+                }
+            }
+        }
+    })
+    .text('Next Step');
+
+//  初期化するボタン
+process.append('button')
+    .style({
+        'background-color': '#a00',
+        'border-radius': '10px',
+        'color': '#ddd',
+        'height': '40px',
+        'margin': '0 auto',
+        'width': '100px',
+        'text-align': 'center'
+    })
+    .on('click', function () {
+        init();
+    })
+    .text('最初から');
+
+init();
+
+var processArray = [
+    '各データに対してランダムでクラスタを割り振る',
+    '各クラスタの重心を計算する(算術平均(ユークリッド距離))',
+    '各データに対して重心が最も近いクラスタに割り当て直す',
+    '移動しなくなったら終了'
+];
+
+process
+    .selectAll('div')
+    .data(processArray)
+    .enter()
+    .append('div')
+    .attr({
+        'id': function(d, i) {
+            return 'process' + i;
+        },
+        'class': 'processDescription'
+    })
+    .style({
+        'border': 'solid 2px #333',
+        'margin': '10px',
+        'height': '70px'
+    })
+    .text(function(d) { return d; })
+
+function init () {
+    dataset = []
+    centroidArr = []
+    centroid = true;
+    first = false;
+    finish = false;
+
+
+    for (var i = 0; i < dataNum; i++) {
+        rand = parseInt((Math.random() * clusterNum), 10);
+        obj = {
+            x: parseInt(normalRand(80 * rand + 100, 20), 10),
+            y: parseInt(normalRand(80 * rand + 100, 20), 10),
+            cluster: parseInt(Math.random() * clusterNum, 10) + 1
+        }
+        console.log('x = ' + obj.x, 'y = ' + obj.y)
+        dataset.push(obj);
     }
-    console.log('x = ' + obj.x, 'y = ' + obj.y)
-    dataset.push(obj);
+
+    svg.selectAll('circle')
+        .remove();
+
+    // データを点として打つ
+    var dataPoint = svg.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr({
+            'class': 'data',
+            cx : function(d){ return d.x; },
+            cy : function(d){ return d.y; },
+            r : 0,
+            // height : function(d){ return d; },
+            'cluster' : function(d){ return d.cluster; }
+            // fill : function(d){ return clusterColor[d.cluster]; }
+        })
+        .transition()
+        // .delay(function(d, i){
+        //     return i * 5;
+        // }) 
+        .duration(150)
+        .ease('bounce')
+        .attr({
+            r: 5
+        });
 }
 
-// データを点として打つ
-var dataPoint = svg.selectAll('rect')
-    .data(dataset)
-    .enter()
-    .append('circle')
-    .attr({
-        'class': 'data',
-        cx : function(d){ return d.x; },
-        cy : function(d){ return d.y; },
-        r : 0,
-        // height : function(d){ return d; },
-        'cluster' : function(d){ return d.cluster; }
-        // fill : function(d){ return clusterColor[d.cluster]; }
-    })
-    .transition()
-    // .delay(function(d, i){
-    //     return i * 5;
-    // }) 
-    .duration(150)
-    .ease('bounce')
-    .attr({
-        r: 5
-    });
-
-var centroidArr = []
-// for (var cnt = 0; cnt < 20; cnt ++) {
 function firstCluster () {
     d3.selectAll('.data')
         .data(dataset)
@@ -87,7 +200,7 @@ function firstCluster () {
 function calCentroid () {
     var dataset = d3.selectAll('.data')[0];    
     console.log(d3.selectAll('.centroid'));
-
+    var notMove = 0;
     
     // 重心の作成
     for (var i = 1; i <= clusterNum; i++) {
@@ -111,11 +224,29 @@ function calCentroid () {
             }),
             "cluster": i
         }
-        centroidArr[i - 1] = c;
-        // console.log(c)
+        if (centroidArr.length >= clusterNum && c.x == centroidArr[i - 1].x && c.y == centroidArr[i - 1].y) {
+            notMove++;
+        }
+
+        if (c.x > 0) {
+            centroidArr[i - 1] = c;
+        }        
     }
 
-    console.log(d3.selectAll('.centroid'));
+    if (notMove >= clusterNum) {
+        console.log('完了');
+        d3.selectAll('.processDescription')
+            .style({
+                'background-color': function (d, i) {
+                    if (i == 3) {
+                        return 'white';
+                    } else {
+                        return 'gray';
+                    }
+                }
+            });
+        finish = true;
+    }
 
     if (d3.selectAll('.centroid')[0].length > 0) {
         console.log('あるよ！');
@@ -159,8 +290,7 @@ function calCentroid () {
                 stroke: "black" 
             });
     }
-        centroid = false;
-
+    centroid = false;
 }
 
 function calDistance() {
@@ -176,14 +306,14 @@ function calDistance() {
                     cluster = 0;
 
                     for (var i = 1; i <= clusterNum; i++) {
-                        console.log(centroidArr[i - 1].x, centroidArr[i - 1].y)
+                        // console.log(centroidArr[i - 1].x, centroidArr[i - 1].y)
                         // ユークリッド距離の計算
                         distance = 
                             Math.sqrt(
                                 Math.pow((centroidArr[i - 1].x - d.x), 2) + Math.pow((centroidArr[i - 1].y - d.y), 2)
                             );
 
-                        console.log(distance);
+                        // console.log(distance);
                         if (min >= distance) {
                             min = distance;
                             cluster = i;
@@ -204,13 +334,13 @@ function calDistance() {
                                 Math.pow((centroidArr[i - 1].x - d.x), 2) + Math.pow((centroidArr[i - 1].y - d.y), 2)
                             );
 
-                        console.log(distance);
+                        // console.log(distance);
                         if (min >= distance) {
                             min = distance;
                             cluster = i;
                         }
                     }
-                    console.log(cluster);
+                    // console.log(cluster);
                     return cluster;
                }
             });
